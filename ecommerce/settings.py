bc372839da61f3bf8edf46ae436ffcd2ff3a1ed9
gabriellguedes <gguedes10@gmail.com>
2022-10-14 14:21:25 -0300

    Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -74,7 +74,6 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -103,7 +102,7 @@ DATABASES = {
         }
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
