7ffca72e09f441a279d96915aa13bf2e3763906f
gabriellguedes <gguedes10@gmail.com>
2022-10-21 12:56:52 -0300

    settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,6 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 
 ROLEPERMISSIONS_MODULE = "ecommerce.roles"
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
