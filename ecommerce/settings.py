481a616586b3c62e40c2deff2b61febe79ccc1dd
gabriellguedes <gguedes10@gmail.com>
2023-07-14 12:50:37 -0300

    alterações

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = config('DEBUG', default=False, cast=bool)
 
 #CONFIGURAÇÕES DO EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'lojamyitens@gmail.com'
+DEFAULT_FROM_EMAIL = 'gguedes10@gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
